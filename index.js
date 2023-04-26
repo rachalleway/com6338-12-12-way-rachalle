49d6ecc8f8c91b7643c6fc1ec1bb3847fbc4b295
Rachalle Way <way.rachalle@gmail.com>
2023-04-26 09:57:22 -0400

    solved

--- index.js
+++ index.js
@@ -26,16 +26,53 @@ class Word {
   }
 
   // implement the guessLetter function:
-  // guessLetter(letter) {}
+guessLetter(letter) {
+  let wordArray = this.word.split('')
+  let displayArray = this.displayWord.split('')
+  let correct = false
+  for (var i = 0; i < wordArray.length; i++){
+    if (wordArray[i] == letter){
+      correct = true
+      this.correctLetters.push(letter)
+      displayArray[i ]= letter
+    }else{
+
+    }
+  }
+  if (correct === true){
+    this.displayWord=displayArray.join('')
+  }else{
+    this.remainingGuesses--
+    this.incorrectLetters.push(letter)
+  }
+}
 
   // implement the updateScreen function:
-  // updateScreen() {}
+updateScreen() {
+  document.getElementById("remaining-guesses").textContent = this.remainingGuesses
+  document.getElementById("incorrect-letters").textContent = this.incorrectLetters
+  document.getElementById("word-to-guess").textContent = this.displayWord
+}
 
   // implement the isGameOver function:
-  // isGameOver() {}
+isGameOver() {
+  if (this.remainingGuesses <= 0 || this.word === this.displayWord){
+    return true
+  }else{
+    return false
+  }
+}
 
   // implement the getWinOrLoss function:
-  // getWinOrLoss() {}
+getWinOrLoss() {
+  if (this.word === this.displayWord && this.remainingGuesses > 0){
+    return 'win'
+  }else if (this.word !== this.displayWord && this.remainingGuesses <= 0){
+    return 'loss'
+  }else{
+    return null
+  }
+}
 }
 
 function newGame() {
